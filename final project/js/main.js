c74ae305d08399a6c8d9fc823cd7e527349034a4
nervousgeek <demented182@hotmail.com>
2016-09-24 16:21:41 +0800

    popot popot popot!!

---
+++ final project/js/main.js
@@ -0,0 +1,40 @@
+$(function() {
+
+	$(window).scroll(function(){
+
+		var wScroll = $(this).scrollTop();
+
+		$('.sanacity').css({
+			'transform' : 'translate(0px, +'+ wScroll /50 +'%)'
+		});
+
+		$('.logo').css({
+			'transform' : 'translate(0px, +'+ wScroll /7 +'%)'
+		});
+
+		$('.dervish').css({
+			'transform' : 'translate(0px, +'+ wScroll /50 +'%)'
+		});
+
+		$('.imageTitle').css({
+			'transform' : 'translate(0px, +'+ wScroll /55 +'%)'
+		});
+
+		$('.subTitle').css({
+			'transform' : 'translate(0px, +'+ wScroll /55 +'%)'
+		});
+
+		$('.wall').css({
+			'transform' : 'translate(0px, -'+ wScroll /300 +'%)'
+		});
+
+		$('.minaretRight').css({
+			'transform' : 'translate(0px, +'+ wScroll /50 +'%)'
+		});
+
+		$('.minaretLeft').css({
+			'transform' : 'translate(0px, +'+ wScroll /20 +'%)'
+		});
+	});
+
+});
